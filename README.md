967de016ce39949f9e111986924e814ee2c9d2e8
hadeel999 <hadeelahmadsaleh@gmail.com>
2022-06-29 20:31:16 +0300

    ChatCord

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # chatcord
+Chat application using Node, Express and Socket.io.
+
+## Resources
+[Realtime Chat With Users & Rooms - Socket.io, Node & Express](https://www.youtube.com/watch?v=jD7FnbI76Hg)
---
+++ server.js
@@ -0,0 +1,47 @@
+'use strict';
+const path=require('path');
+const http=require('http');
+const express=require('express');
+const socketio=require('socket.io');
+const formatMessages=require("./utils/messages");
+const {userJoin,getCurrentUser,userLeave,getRoomUsers}=require("./utils/users");
+
+const app=express();
+const server=http.createServer(app);
+const io=socketio(server);
+
+app.use(express.static(path.join(__dirname,'public')));
+
+const botName='ChatCord Bot';
+
+io.on('connection',(socket)=>{
+
+    socket.on('joinRoom',({username,room})=>{
+        const user = userJoin(socket.id,username,room);
+        socket.join(user.room);
+        socket.emit('message',formatMessages(botName,"Welcome to ChatCord!"));
+        socket.broadcast.to(user.room).emit('message',formatMessages(botName,`${user.username} has joined the chat.`));  
+
+        io.to(user.room).emit('roomUsers',{room:user.room, users:getRoomUsers(user.room)})
+    })
+
+    socket.on('chatMessage',(msg)=>{
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit('message',formatMessages(user.username,msg));
+    })
+
+    socket.on('disconnect',()=>{
+        const user=userLeave(socket.id);
+        if(user){
+            io.to(user.room).emit('message',formatMessages(botName,`${user.username} has left the chat.`));
+            io.to(user.room).emit('roomUsers',{room:user.room, users:getRoomUsers(user.room)})
+        }
+
+    })
+
+})
+const PORT=process.env.PORT || 3000;
+
+server.listen(PORT,() =>{
+    console.log(`Server running on Port ${PORT}`);
+})
